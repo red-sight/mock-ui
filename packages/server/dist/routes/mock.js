4a8c72d7c1c8ab2758898471b6567b07b5aa4e8a
redsight <malahov.dmitry@gmail.com>
2021-11-07 23:47:26 +0300

    fix(git): clean repo

--- packages/server/dist/routes/mock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _mock = _interopRequireDefault(require("../models/mock"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {};
